fc7df2f514f0da4c7c7a2a3a91127aaf777effb4
cullenr <rory.cullen@imagination.com>
2019-01-04 15:46:23 +0000

    ADDED better texture filtering for 2d stuff and some formatting

--- gfx/gfx.go
+++ gfx/gfx.go
@@ -130,25 +130,27 @@ func LoadTexture(file string) (uint32, error) {
 	if err != nil {
 		return 0, fmt.Errorf("texture %q not found on disk: %v", file, err)
 	}
+
 	img, _, err := image.Decode(imgFile)
 	if err != nil {
 		return 0, err
 	}
 
 	rgba := image.NewRGBA(img.Bounds())
-	if rgba.Stride != rgba.Rect.Size().X*4 {
+	if rgba.Stride != rgba.Rect.Size().X * 4 {
 		return 0, fmt.Errorf("unsupported stride")
 	}
+
 	draw.Draw(rgba, rgba.Bounds(), img, image.Point{0, 0}, draw.Src)
 
 	var texture uint32
 	gl.GenTextures(1, &texture)
 	gl.ActiveTexture(gl.TEXTURE0)
 	gl.BindTexture(gl.TEXTURE_2D, texture)
-	gl.TexParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR)
-	gl.TexParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR)
-	gl.TexParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE)
-	gl.TexParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE)
+	gl.TexParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST)
+	gl.TexParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST)
+	gl.TexParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.REPEAT)
+	gl.TexParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.REPEAT)
 	gl.TexImage2D(
 		gl.TEXTURE_2D,
 		0,
--- main.go
+++ main.go
@@ -1,11 +1,11 @@
 package main
 
 import (
-	_ "image/png"
-	"log"
 	"github.com/cullenr/yanhg/gfx"
 	"github.com/go-gl/gl/v4.1-core/gl"
 	"github.com/go-gl/glfw/v3.2/glfw"
+	_ "image/png"
+	"log"
 )
 
 const windowWidth = 800
@@ -40,32 +40,33 @@ void main()
     color = texture(tex, uv);
 }
 ` + "\x00"
+
 func main() {
-    window, err := gfx.InitWindow(windowWidth, windowHeight)
+	window, err := gfx.InitWindow(windowWidth, windowHeight)
 	if err != nil {
 		log.Fatalln(err)
 	}
 	defer gfx.Destroy()
-
+	// TODO : these will be loaded when the level loads
 	program, err := gfx.ProgramFromSource(vertexShader, fragmentShader)
 	if err != nil {
 		log.Fatalln(err)
 	}
 
+	// TODO : these will be loaded when the level loads
 	texture, err := gfx.LoadTexture("square.png")
 	if err != nil {
 		log.Fatalln(err)
 	}
 
-    sprite := gfx.NewSprite(program, []uint32{texture})
+	sprite := gfx.NewSprite(program, []uint32{texture})
 
 	for !window.ShouldClose() {
 		gl.Clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT)
 
-        sprite.Draw()
+		sprite.Draw()
 
 		window.SwapBuffers()
 		glfw.PollEvents()
 	}
 }
-
